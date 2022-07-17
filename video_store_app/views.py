0d8ed0d417e87901f40f416d1922447ffeeb576f
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-07-16 22:22:11 -0500

    some stuff

--- video_store_app/views.py
+++ video_store_app/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 def index(request):
     response = render(request, 'pages/index.html')
     return response
+
+def get_videos(request):
+    pass
+
+def get_customers(request):
+    pass
+
+def get_customer(request):
+    pass
+
+def add_customer(request):
+    pass
+
+def rent_video(request):
+    pass
+
+def return_video(request):
+    pass
